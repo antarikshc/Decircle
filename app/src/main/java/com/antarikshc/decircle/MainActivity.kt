34efe5f20df956b09ea6bd08799a3d5f634b4f85
Antariksh Chavan <antarikshc@gmail.com>
2020-09-05 21:18:12 +0530

    chore: Sample UI
    Signed-off-by: Antariksh C <antarikshc@gmail.com>

--- app/src/main/java/com/antarikshc/decircle/MainActivity.kt
+++ app/src/main/java/com/antarikshc/decircle/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.antarikshc.decircle
 
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.EditorInfo
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        edit_slider_value.setOnEditorActionListener { _, id, _ ->
+            if (id == EditorInfo.IME_ACTION_DONE) {
+                animate()
+                return@setOnEditorActionListener true
+            }
+            return@setOnEditorActionListener false
+        }
+    }
+
+    fun animate(v: View? = null) {
+        slider.setProgress(edit_slider_value.text.toString().toInt())
     }
 }
