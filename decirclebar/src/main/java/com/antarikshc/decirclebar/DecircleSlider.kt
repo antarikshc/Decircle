e3b9edc90088715c699f8f7c8367a5cc0de6cbc7
Antariksh Chavan <antarikshc@gmail.com>
2020-09-05 15:28:46 +0530

    feat: Draw circle for background
    Signed-off-by: Antariksh C <antarikshc@gmail.com>

--- decirclebar/src/main/java/com/antarikshc/decirclebar/DecircleSlider.kt
+++ decirclebar/src/main/java/com/antarikshc/decirclebar/DecircleSlider.kt
@@ -1,8 +1,12 @@
 package com.antarikshc.decirclebar
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import kotlin.math.min
 
 class DecircleSlider : View {
 
@@ -22,8 +26,52 @@ class DecircleSlider : View {
         init(context, attrs)
     }
 
+    /**   Props   */
+
+    // Center points of Views
+    private var centerX = 0
+    private var centerY = 0
+
+    // Paints
+    private var backdropPaint = Paint()
 
     fun init(context: Context, attrs: AttributeSet? = null) {
+        backdropPaint = backdropPaint.apply {
+            color = Color.GRAY
+            style = Paint.Style.STROKE
+            strokeWidth = 10F
+        }
     }
+
+    /**
+     * Called every time View size changes
+     * Initial call with old values 0
+     */
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        // Take min to keep content centered. Ignore the diff between W and H
+        val min = min(w, h)
+
+        // Get all sides of View
+        val left = (w - min) / 2
+        val top = (h - min) / 2
+        val right = left + min
+        val bottom = top + min
+
+        // Get center points of View
+        centerX = right / 2 + (w - right) / 2
+        centerY = bottom / 2 + (h - bottom) / 2
+
+        super.onSizeChanged(w, h, oldw, oldh)
+    }
+
+
+    override fun onDraw(canvas: Canvas?) {
+        canvas?.apply {
+
+            drawCircle(centerX.toFloat(), centerY.toFloat(), 300F, backdropPaint)
+
+        }
+    }
+
 
 }
