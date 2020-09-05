b87b7896328d375d9751d620cf5e93a191350199
Antariksh Chavan <antarikshc@gmail.com>
2020-09-05 16:58:35 +0530

    feat: Draw Picker
    Signed-off-by: Antariksh C <antarikshc@gmail.com>

--- decirclebar/src/main/java/com/antarikshc/decirclebar/DecircleSlider.kt
+++ decirclebar/src/main/java/com/antarikshc/decirclebar/DecircleSlider.kt
@@ -6,9 +6,14 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
-import kotlin.math.min
+import kotlin.math.*
 
 class DecircleSlider : View {
+
+    companion object {
+        private const val MAX_PROGRESS = 100
+        private const val DEGREE_CONVERSION = MAX_PROGRESS / 360F
+    }
 
     constructor(context: Context) : super(context) {
         init(context)
@@ -28,22 +33,49 @@ class DecircleSlider : View {
 
     /**   Props   */
 
+    private var progress = 0
+    private val progressInDegree = progress / DEGREE_CONVERSION
+
+
     // Center points of Views
     private var centerX = 0
     private var centerY = 0
 
     private var diameter = 0
 
-    private var backdropWidth = 10F
 
-    // Paints
+    // Angles
+    // we define picker and slider position by angle
+    private var initAngle = PI / 2 - (progressInDegree * PI) / 180
+
+    // Progress angle
+    private var angle = initAngle
+
+
+    // Backdrop props
+    private var backdropWidth = 10F
     private var backdropPaint = Paint()
+
+
+    // Picker props
+    private var pickerX = 0
+    private var pickerY = 0
+    private var pickerWidth = 50F
+    private var pickerPaint = Paint()
+
+    private var maxWidth = max(backdropWidth, pickerWidth)
+
 
     fun init(context: Context, attrs: AttributeSet? = null) {
         backdropPaint = backdropPaint.apply {
             color = Color.GRAY
             style = Paint.Style.STROKE
             strokeWidth = backdropWidth
+        }
+
+        pickerPaint = pickerPaint.apply {
+            color = Color.RED
+            style = Paint.Style.FILL
         }
     }
 
@@ -66,8 +98,12 @@ class DecircleSlider : View {
         centerY = bottom / 2 + (h - bottom) / 2
 
         // Total height or width is diameter
-        // Minus the stroke width
-        diameter = min - backdropWidth.toInt()
+        // Minus the max stroke/picker width
+        diameter = min - maxWidth.toInt()
+
+        // Calc picker distance
+        pickerX = centerX + ((diameter / 2F) * cos(angle)).toInt()
+        pickerY = centerY - ((diameter / 2F) * sin(angle)).toInt()
 
         super.onSizeChanged(w, h, oldw, oldh)
     }
@@ -76,10 +112,13 @@ class DecircleSlider : View {
     override fun onDraw(canvas: Canvas?) {
         canvas?.apply {
 
+            // Draw backdrop
             drawCircle(centerX.toFloat(), centerY.toFloat(), diameter / 2F, backdropPaint)
+
+            // Draw Picker
+            drawCircle(pickerX.toFloat(), pickerY.toFloat(), pickerWidth / 2F, pickerPaint)
 
         }
     }
-
 
 }
